cf8c755299dd8eed0a6bbb8dcbeba5cae2774fe9
Aleksei <dulittlefamily@yandex.ru>
2024-03-21 16:56:31 +0300

    Hometask 5. Created class ViewClassEng. Interface iGetView emplements in ViewClassEng.

---
+++ MVCStudentsApp/src/View/ViewClassEng.java
@@ -0,0 +1,25 @@
+package View;
+
+import Controller.Interfaces.iGetView;
+import Model.Domain.Student;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ViewClassEng implements iGetView {
+    public void printAllStudents(List<Student> students) {
+        System.out.println("************ List of students **************");
+        for (Student s : students) {
+            System.out.println(s);
+        }
+        System.out.println("********************************************");
+    }
+
+    @Override
+    public String prompt(String msg) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(msg);
+        return in.nextLine();
+    }
+
+}
